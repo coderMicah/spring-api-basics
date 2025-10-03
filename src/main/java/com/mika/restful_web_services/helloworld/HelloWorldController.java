472dea2ce3dc9ef8b4bb3e2d1ccb11addd805763
Micah <ismiki977@gmail.com>
2025-10-03 17:43:01 +0300

    feat:Internationalization(messages.properties)

--- src/main/java/com/mika/restful_web_services/helloworld/HelloWorldController.java
+++ src/main/java/com/mika/restful_web_services/helloworld/HelloWorldController.java
@@ -1,11 +1,21 @@
 package com.mika.restful_web_services.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     // @RequestMapping(path = "/hello-world", method = RequestMethod.GET)
     @GetMapping(path = "/hello-world")
@@ -21,5 +31,12 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world/{name}")
     public HelloWorldBean helloBeanPath(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello, %s", name));
+    }
+
+    @GetMapping(path = "/hello-world/i18n")
+    public String helloWorldInternationalization() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+
     }
 }
